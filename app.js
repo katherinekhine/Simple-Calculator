bb540c73a9fce29a575d1f667148f8d8caed7717
katherinekhine <khaingkatherine@gmail.com>
2024-08-18 18:52:00 +0630

    update index.html and add app.ts

---
+++ app.js
@@ -0,0 +1,14 @@
+var number1 = document.getElementById("num1");
+var number2 = document.getElementById("num2");
+var AddBtn = document.getElementById("Add");
+var SubBtn = document.getElementById("Sub");
+var MultBtn = document.getElementById("Mult");
+var DivBtn = document.getElementById("Divide");
+var printResult = document.getElementById("result");
+function AddNumber() {
+    var a = parseFloat(number1.value);
+    var b = parseFloat(number2.value);
+    var result = a + b;
+    printResult.textContent = result.toString();
+}
+AddBtn.addEventListener("click", AddNumber);
---
+++ app.ts
@@ -0,0 +1,17 @@
+const number1 = document.getElementById("num1") as HTMLButtonElement;
+const number2 = document.getElementById("num2") as HTMLButtonElement;
+
+const AddBtn = document.getElementById("Add") as HTMLButtonElement;
+const SubBtn = document.getElementById("Sub") as HTMLButtonElement;
+const MultBtn = document.getElementById("Mult") as HTMLButtonElement;
+const DivBtn = document.getElementById("Divide") as HTMLButtonElement;
+
+const printResult = document.getElementById("result") as HTMLButtonElement;
+
+function AddNumber() {
+  let a = parseFloat(number1.value);
+  let b = parseFloat(number2.value);
+  let result = a + b;
+  printResult.textContent = result.toString();
+}
+AddBtn.addEventListener("click", AddNumber);
